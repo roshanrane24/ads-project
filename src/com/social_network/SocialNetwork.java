7c4fb10b744d9f2f0f2592c9d0135170fb283bc5
Roshan Rane <roshan.rane97@gmail.com>
2022-01-08 19:47:56 +0530

    Social Network API

--- src/com/social_network/SocialNetwork.java
+++ src/com/social_network/SocialNetwork.java
@@ -5,30 +5,51 @@ import com.graph.GraphInterface;
 public class SocialNetwork {
 	private GraphInterface<User> graph;
 
-	public SocialNetwork(GraphInterface<User> graph) {
-		this.graph = graph;
+	public SocialNetwork() {
+		this.graph = null;
 	}
 	
+	// add a user in network
 	public void addUser(User user) {
 	}
 	
-	public User removeUser() {
+	// remove a user in network
+	public User removeUser(int id) {
 		return null;
 	}
 	
+	// show all users in network
 	public void listUsers() {
 		
 	}
 	
+	// get a user by Id
 	public User getUser(int id) {
 		return null;
 	}
 	
-	public void addFriends(User first, User Second) {
+	// add connection between 2 users in network
+	public void addConnection(User first, User Second) {
 		
 	}
 
-	public void removeFriends(User first, User Second) {
+	// remove connection between 2 users in network
+	public void removeConnection(User first, User Second) {
 		
+	}
+	
+	// Display all connections of user
+	public void showConnection(User user) {
+		
+	}
+
+	// Display all mutual connections of users
+	public void showMutualConnection(User user1, User user2) {
+		
+	}
+	
+	// Suggest new possible connections for user
+	public User[] suggestConnections(User user) {
+		return null;
 	}
 }
