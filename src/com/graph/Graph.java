4932c1b46f3c5a339892bb4dd35e6ad5430241d5
Roshan Rane <roshan.rane97@gmail.com>
2022-01-13 02:09:56 +0530

    # Fixes & Re-implemented BFS method in Graph
    - Initialized HashSet with checks in addEdge.
    - Added checks in removeEdge method.
    - Fixed ClassCastException in removeNode
    - Fixed BFS method & added checks to overloaded methods
    - Changed test graph size.
    - Fixed tests methods for new data.

--- src/com/graph/Graph.java
+++ src/com/graph/Graph.java
@@ -4,20 +4,23 @@
 package com.graph;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 import com.hashtable.HashMap;
 import com.hashtable.HashMapInterface;
 import com.hashtable.HashSetInterface;
-import com.linked_list.LinkedListInterface;
+import com.hashtable.HashSett;
 import com.utils.PrimaryKey;
 
-public class Graph<K extends Comparable<K>, V extends PrimaryKey<K>> implements GraphInterface<K,V>{
+import static com.utils.CommonUtils.*;
+
+public class Graph<K extends Comparable<K>, V extends PrimaryKey<K>> implements GraphInterface<K, V> {
 
 	private HashMapInterface<K, V> vertexlist;
 	private HashMapInterface<K, HashSetInterface<K>> edgeList;
-	
+
 	public Graph() {
 		vertexlist = new HashMap<>();
 		edgeList = new HashMap<>();
@@ -37,7 +40,18 @@ public class Graph<K extends Comparable<K>, V extends PrimaryKey<K>> implements 
 
 		if (vertexlist.get(end) == null)
 			throw new RuntimeException("No element stored for key " + end);
-		
+
+		// check if index start and end element initialized
+		HashSetInterface<K> set1 = edgeList.get(start);
+
+		if (set1 == null)
+			edgeList.put(start, new HashSett<>());
+
+		HashSetInterface<K> set2 = edgeList.get(end);
+
+		if (set2 == null)
+			edgeList.put(end, new HashSett<>());
+
 		// add edge from start to end
 		edgeList.get(start).add(end);
 
@@ -53,7 +67,18 @@ public class Graph<K extends Comparable<K>, V extends PrimaryKey<K>> implements 
 
 		if (vertexlist.get(end) == null)
 			throw new RuntimeException("No element stored for key " + end);
-		
+
+		// check if index start and end element initialized
+		HashSetInterface<K> set1 = edgeList.get(start);
+
+		if (set1 == null)
+			throw new RuntimeException("No edges are stored for node " + start);
+
+		HashSetInterface<K> set2 = edgeList.get(end);
+
+		if (set2 == null)
+			throw new RuntimeException("No edges are stored for node " + start);
+
 		// remove edge from start to end
 		edgeList.get(start).remove(end);
 
@@ -61,28 +86,29 @@ public class Graph<K extends Comparable<K>, V extends PrimaryKey<K>> implements 
 		edgeList.get(end).remove(start);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public V removeNode(V element) {
 		K key = element.getKey();
 		// check if elements exist for given key
 		if (vertexlist.get(key) == null)
 			return null;
-		
+
 		// remove Node
 		vertexlist.remove(key);
-		
+
 		// get neighbors
-	
-		K[] neighbors =	edgeList.get(key).values();
-		
+
+		Object[] neighbors = edgeList.get(key).values();
+
 		// remove mode connections
 		edgeList.remove(key);
-		
+
 		// remove neighbors connection
-		for (K k : neighbors) {
-			edgeList.get(k).remove(key);
+		for (Object k : neighbors) {
+			edgeList.get((K)k).remove(key);
 		}
-		
+
 		return element;
 	}
 
@@ -105,7 +131,12 @@ public class Graph<K extends Comparable<K>, V extends PrimaryKey<K>> implements 
 
 	@Override
 	public V[] BFS() {
-		return BFS(((HashMap<K, V>)vertexlist).getFirst(), -2);
+		V node = ((HashMap<K, V>) vertexlist).getFirst();
+		
+		if (node == null)
+			return null;
+		
+		return BFS(node, -2);
 	}
 
 	@Override
@@ -113,43 +144,76 @@ public class Graph<K extends Comparable<K>, V extends PrimaryKey<K>> implements 
 		return BFS(node, -2);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public V[] BFS(V node, int depth) {
-		LinkedListInterface<V> result= null; // TODO instance of LL
+		// Store result
+		V[] result = null;
+		int count = 0;
+
+		// map to check is node visited
 		HashMapInterface<K, Boolean> isVisited = new HashMap<K, Boolean>();
-	
+		K[] keys = vertexlist.keys();
+
+		for (int i = 0; i < keys.length; i++) {
+			isVisited.put(keys[i], false);
+		}
+		
+		result = (V[]) Array.newInstance(vertexlist.get(keys[0]).getClass(), vertexlist.size());
+
+		// create & add node to queue
 		Queue<K> queue = new LinkedList<>();
 		queue.add(node.getKey());
+
+		// add null to queue to indicate end of depth
 		queue.add(null);
-		
-		while (!queue.isEmpty() && depth + 1 != 0) {
+
+		// until queue is empty
+		while (!queue.isEmpty()) {
+			// get element from queue
 			K current = queue.remove();
-			
-			if (current == null && !queue.isEmpty()) {
+
+			// check depth
+			if (depth == 0)
+				break;
+
+			// when within depth
+			if (current != null) {
+				if (!isVisited.get(current)) {
+					// mark current visited
+					isVisited.put(current, true);
+					
+					// process node
+					result[count++] = vertexlist.get(current);
+					
+					// get next edges
+					HashSetInterface<K> edgeSet = edgeList.get(current);
+					
+					
+					if (edgeSet != null) {
+						Object[] edges = edgeSet.values();
+						Arrays.sort(edges);
+						for (int i = 0; i < edges.length; i++) {
+							if (!isVisited.get((K) edges[i]))
+								queue.add((K) edges[i]);
+						} 
+					}
+				}
+
+			} else {
 				// decrease depth
 				depth--;
-				queue.add(null);
-			} else {
-				// mark node visited
-				if (isVisited.get(current) == false)
-					isVisited.put(current, true);
-				
-				result.add(vertexlist.get(current));
-				
-				for (K k : edgeList.get(current).values()) {
-					queue.add(k);
-				} 
+				// to check last depth
+				if (!queue.isEmpty())
+					queue.add(null);
 			}
+
 		}
-		
-		@SuppressWarnings("unchecked")
-		V[] res = (V[]) Array.newInstance(node.getClass(), result.size());
-		
-		for (int i = 0; i < res.length; i++) {
-			res[i] = result.get(i);
-		}
-		
-		return res;
+		return squashArray(result, vertexlist.get(keys[0]).getClass());
+	}
+	
+	public V[] getVertexlist() {
+		return vertexlist.values();
 	}
 
 }
--- test/com/graph/GraphTest.java
+++ test/com/graph/GraphTest.java
@@ -3,6 +3,7 @@ package com.graph;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,8 +15,8 @@ class GraphTest {
 	static User u1, u2, u3, u4, u5, u6, u7, u8, u9, u10, u11, u12, u13, u14, u15, u16, u17, u18, u19, u20, u21, u22,
 			u23, u24, u25, u26;
 
-	@BeforeEach
-	void setUp() {
+	@BeforeAll
+	static void setUpBeforeAll() {
 		u1 = new User("1", Gender.MALE);
 		u2 = new User("2", Gender.MALE);
 		u3 = new User("3", Gender.MALE);
@@ -42,156 +43,169 @@ class GraphTest {
 		u24 = new User("24", Gender.MALE);
 		u25 = new User("25", Gender.MALE);
 		u26 = new User("26", Gender.MALE);
+	}
 
-		graph.removeNode(u1);
-		graph.removeNode(u2);
-		graph.removeNode(u3);
-		graph.removeNode(u4);
-		graph.removeNode(u5);
-		graph.removeNode(u6);
-		graph.removeNode(u7);
-		graph.removeNode(u8);
-		graph.removeNode(u9);
-		graph.removeNode(u10);
-		graph.removeNode(u11);
-		graph.removeNode(u12);
-		graph.removeNode(u13);
-		graph.removeNode(u14);
-		graph.removeNode(u15);
-		graph.removeNode(u16);
-		graph.removeNode(u17);
-		graph.removeNode(u18);
-		graph.removeNode(u19);
-		graph.removeNode(u20);
-		graph.removeNode(u21);
-		graph.removeNode(u22);
-		graph.removeNode(u23);
-		graph.removeNode(u24);
-		graph.removeNode(u25);
-		graph.removeNode(u26);
+	@BeforeEach
+	void setUp() {
+		graph = new Graph<>();
 
-		graph.addEdge(8, 2);
-		graph.addEdge(4, 14);
-		graph.addEdge(25, 20);
-		graph.addEdge(4, 22);
-		graph.addEdge(13, 24);
-		graph.addEdge(2, 12);
-		graph.addEdge(6, 16);
-		graph.addEdge(17, 18);
-		graph.addEdge(1, 6);
-		graph.addEdge(18, 20);
-		graph.addEdge(18, 3);
-		graph.addEdge(23, 24);
-		graph.addEdge(6, 15);
-		graph.addEdge(10, 13);
-		graph.addEdge(6, 21);
-		graph.addEdge(12, 13);
-		graph.addEdge(10, 14);
-		graph.addEdge(10, 22);
-		graph.addEdge(13, 23);
-		graph.addEdge(13, 14);
-		graph.addEdge(1, 8);
-		graph.addEdge(1, 2);
-		graph.addEdge(9, 14);
-		graph.addEdge(17, 16);
-		graph.addEdge(11, 2);
+		graph.addNode(u1);
+		graph.addNode(u2);
+		graph.addNode(u3);
+		graph.addNode(u4);
+		graph.addNode(u5);
+		graph.addNode(u6);
+		graph.addNode(u7);
+		graph.addNode(u8);
+		graph.addNode(u9);
+//		graph.addNode(u10);
+//		graph.addNode(u11);
+//		graph.addNode(u12);
+//		graph.addNode(u13);
+//		graph.addNode(u14);
+//		graph.addNode(u15);
+//		graph.addNode(u16);
+//		graph.addNode(u17);
+//		graph.addNode(u18);
+//		graph.addNode(u19);
+//		graph.addNode(u20);
+//		graph.addNode(u21);
+//		graph.addNode(u22);
+//		graph.addNode(u23);
+//		graph.addNode(u24);
+//		graph.addNode(u25);
+//		graph.addNode(u26);
+
+//		graph.addEdge(8, 2);
+//		graph.addEdge(4, 14);
+//		graph.addEdge(25, 20);
+//		graph.addEdge(4, 22);
+//		graph.addEdge(13, 24);
+//		graph.addEdge(2, 12);
+//		graph.addEdge(6, 16);
+//		graph.addEdge(17, 18);
+//		graph.addEdge(1, 6);
+//		graph.addEdge(18, 20);
+//		graph.addEdge(18, 3);
+//		graph.addEdge(23, 24);
+//		graph.addEdge(6, 15);
+//		graph.addEdge(10, 13);
+//		graph.addEdge(6, 21);
+//		graph.addEdge(12, 13);
+//		graph.addEdge(10, 14);
+//		graph.addEdge(10, 22);
+//		graph.addEdge(13, 23);
+//		graph.addEdge(13, 14);
+//		graph.addEdge(1, 8);
+//		graph.addEdge(1, 2);
+//		graph.addEdge(9, 14);
+//		graph.addEdge(17, 16);
+//		graph.addEdge(11, 2);
+//		graph.addEdge(9, 2);
+//		graph.addEdge(5, 3);
+//		graph.addEdge(22, 23);
+//		graph.addEdge(19, 2);
+//		graph.addEdge(5, 4);
+//		graph.addEdge(6, 4);
+//		graph.addEdge(2, 3);
+//		graph.addEdge(25, 4);
+//		graph.addEdge(20, 5);
+//		graph.addEdge(17, 11);
+//		graph.addEdge(10, 12);
+//		graph.addEdge(8, 9);
+//		graph.addEdge(8, 12);
+//		graph.addEdge(26, 8);
+//		graph.addEdge(16, 18);
+//		graph.addEdge(5, 19);
+//		graph.addEdge(18, 19);
+//		graph.addEdge(2, 5);
+//		graph.addEdge(10, 4);
+//		graph.addEdge(14, 24);
+//		graph.addEdge(1, 7);
+//		graph.addEdge(12, 14);
+//		graph.addEdge(14, 22);
+//		graph.addEdge(8, 7);
+//		graph.addEdge(4, 9);
+//		graph.addEdge(5, 25);
+//		graph.addEdge(8, 10);
+//		graph.addEdge(25, 19);
+//		graph.addEdge(6, 3);
+//		graph.addEdge(25, 9);
+//		graph.addEdge(25, 22);
+//		graph.addEdge(9, 5);
+//		graph.addEdge(18, 11);
+//		graph.addEdge(16, 3);
+//		graph.addEdge(9, 10);
+//		graph.addEdge(2, 4);
+
+		graph.addEdge(1, 3);
+		graph.addEdge(1, 4);
+		graph.addEdge(1, 9);
+		graph.addEdge(3, 2);
+		graph.addEdge(3, 6);
 		graph.addEdge(9, 2);
-		graph.addEdge(5, 3);
-		graph.addEdge(22, 23);
-		graph.addEdge(19, 2);
-		graph.addEdge(5, 4);
-		graph.addEdge(6, 4);
-		graph.addEdge(2, 3);
-		graph.addEdge(25, 4);
-		graph.addEdge(20, 5);
-		graph.addEdge(17, 11);
-		graph.addEdge(10, 12);
-		graph.addEdge(8, 9);
-		graph.addEdge(8, 12);
-		graph.addEdge(26, 8);
-		graph.addEdge(16, 18);
-		graph.addEdge(5, 19);
-		graph.addEdge(18, 19);
-		graph.addEdge(2, 5);
-		graph.addEdge(10, 4);
-		graph.addEdge(14, 24);
-		graph.addEdge(1, 7);
-		graph.addEdge(12, 14);
-		graph.addEdge(14, 22);
-		graph.addEdge(8, 7);
-		graph.addEdge(4, 9);
-		graph.addEdge(5, 25);
-		graph.addEdge(8, 10);
-		graph.addEdge(25, 19);
-		graph.addEdge(6, 3);
-		graph.addEdge(25, 9);
-		graph.addEdge(25, 22);
+		graph.addEdge(9, 4);
 		graph.addEdge(9, 5);
-		graph.addEdge(18, 11);
-		graph.addEdge(16, 3);
-		graph.addEdge(9, 10);
-		graph.addEdge(2, 4);
+		graph.addEdge(4, 5);
+		graph.addEdge(4, 6);
+		graph.addEdge(5, 6);
+		graph.addEdge(5, 8);
+		graph.addEdge(6, 7);
+		graph.addEdge(7, 2);
 	}
 
 	@Test
 	void test1() {
-		assertArrayEquals(new User[] { u17, u16, u18, u11, u6, u3, u20, u19, u2, u15, u21, u1, u4, u5, u25, u9, u8, u12,
-				u7, u22, u10, u14, u26, u13, u23, u24 }, graph.BFS(u17));
+//		assertArrayEquals(new User[] { u17, u16, u18, u11, u6, u3, u20, u19, u2, u15, u21, u1, u4, u5, u25, u9, u8, u12,
+//				u7, u22, u10, u14, u26, u13, u23, u24 }, graph.BFS(u17));
+		assertArrayEquals(new User[] { u1, u3, u4, u9, u2, u6, u5, u7, u8 }, graph.BFS(u1));
 	}
 
 	@Test
 	void test2() {
-		assertArrayEquals(new User[] { u15, u6, u16, u21, u1, u4, u3, u17, u18, u8, u7, u2, u9, u22, u25, u10, u5, u14,
-				u11, u20, u19, u12, u26, u23, u13, u24 }, graph.BFS(u15));
+		assertArrayEquals(new User[] { u8, u5, u4, u6, u9, u1, u3, u7, u2}, graph.BFS(u8));
 	}
 
 	@Test
 	void test3() {
-		assertArrayEquals(new User[] { u17, u16, u18, u11 }, graph.BFS(u17, 1));
+		assertArrayEquals(new User[] { u1, u3, u4, u9 }, graph.BFS(u1, 2));
 	}
 
 	@Test
 	void test4() {
-		assertArrayEquals(new User[] { u11 }, graph.BFS(u11, 0));
+		assertArrayEquals(new User[] { u1 }, graph.BFS(u1, 1));
 	}
 
 	@Test
 	void test5() {
-		graph.removeNode(u2);
-		assertArrayEquals(new User[] { u1, u8, u7, u6, u9, u10, u12, u26, u16, u15, u21, u4, u3, u25, u5, u14, u22, u13,
-				u17, u18, u20, u19, u24, u23, u11 }, graph.BFS(u1));
+		assertArrayEquals(new User[] {  }, graph.BFS(u11, 0));
 	}
 
 	@Test
 	void test6() {
-		graph.removeNode(u14);
-		assertArrayEquals(new User[] { u1, u8, u7, u2, u6, u9, u10, u12, u26, u11, u5, u19, u4, u3, u16, u15, u21, u25,
-				u22, u13, u17, u18, u20, u23, u24 }, graph.BFS(u1));
+		graph.removeNode(u2);
+		assertArrayEquals(new User[] { u1, u3, u4, u9, u6, u5, u7, u8 }, graph.BFS(u1));
 	}
 
 	@Test
 	void test7() {
-		assertEquals(graph.getNode(20), u20);
+		assertEquals(u5, graph.getNode(5));
 	}
 
 	@Test
 	void test8() {
-		graph.removeEdge(6, 15);
-		assertArrayEquals(new User[] { u17, u16, u18, u11, u6, u3, u20, u19, u2, u21, u1, u4, u5, u25, u9, u8, u12, u7,
-				u22, u10, u14, u26, u13, u23, u24 }, graph.BFS(u17));
+		assertEquals(null, graph.getNode(15));
 	}
 
 	@Test
 	void test9() {
-		graph.removeEdge(4, 10);
-		assertArrayEquals(new User[] { u17, u16, u18, u11, u6, u3, u20, u19, u2, u15, u21, u1, u4, u5, u25, u9, u8, u12,
-				u7, u22, u14, u10, u26, u13, u23, u24 }, graph.BFS(u17));
+		graph.removeEdge(1, 9);
+		assertArrayEquals(new User[] {u1, u3, u4, u2, u6, u5, u9, u7, u8}, graph.BFS(u1));
 	}
 
 	@Test
 	void test10() {
-		graph.addEdge(3, 19);
-		assertArrayEquals(new User[] { u17, u16, u18, u11, u6, u3, u20, u19, u2, u15, u21, u1, u4, u5, u25, u9, u8, u12,
-				u7, u22, u10, u14, u26, u13, u23, u24 }, graph.BFS(u17));
+		graph.addEdge(1, 8);
+		assertArrayEquals(new User[] {u1, u3, u4, u8, u9, u2, u6, u5, u7}, graph.BFS(u1));
 	}
 }
