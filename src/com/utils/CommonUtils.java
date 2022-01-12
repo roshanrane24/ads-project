98c23defb48e8d5f90b0f81ac2049963be3be50e
Roshan Rane <roshan.rane97@gmail.com>
2022-01-13 01:42:52 +0530

    # New Utilities &
    - Added common utility method for array trimming
    - Fixed add user gender for female input.
    - Added a method to populating existing network

---
+++ src/com/utils/CommonUtils.java
@@ -0,0 +1,28 @@
+package com.utils;
+
+import java.lang.reflect.Array;
+
+public class CommonUtils {
+	/*
+	 * Return Trimmed array removing null value at end
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T[] squashArray (T[] arr, Class<?> c) {
+		if (arr[0] == null)
+			return (T[]) Array.newInstance(c, 0);;
+		
+		int count = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != null) count++;
+		}
+		
+		T[] result = (T[]) Array.newInstance(c, count);
+		
+		for (int i = 0; i < count; i++) {
+			result[i] = arr[i];
+		}
+		
+		return result;
+	}
+}
--- src/com/utils/SocialNetworkUtils.java
+++ src/com/utils/SocialNetworkUtils.java
@@ -22,7 +22,7 @@ public class SocialNetworkUtils {
 		case "f":
 		case "female":
 		case "Female":
-			network.addUser(new User(name, Gender.MALE));
+			network.addUser(new User(name, Gender.FEMALE));
 			break;
 
 		default:
@@ -117,6 +117,99 @@ public class SocialNetworkUtils {
 			throw new Exception("User with id " + id + " does not exist.");
 
 		network.suggestConnections(u);
+	}
+
+	public static void populateNetwork(SocialNetwork network) throws Exception {
+		addAUserToNetwork("Mona", "F", network);
+		addAUserToNetwork("Scott", "M", network);
+		addAUserToNetwork("Jimmy", "M", network);
+		addAUserToNetwork("Darryl", "M", network);
+		addAUserToNetwork("Elbert", "M", network);
+		addAUserToNetwork("Amber", "F", network);
+		addAUserToNetwork("Paula", "F", network);
+		addAUserToNetwork("Rochelle", "F", network);
+		addAUserToNetwork("Vera", "F", network);
+		addAUserToNetwork("Rosemarie", "F", network);
+		addAUserToNetwork("Mindy", "F", network);
+		addAUserToNetwork("Jorge", "M", network);
+		addAUserToNetwork("Gladys", "M", network);
+		addAUserToNetwork("Larry", "M", network);
+		addAUserToNetwork("Sue", "F", network);
+		addAUserToNetwork("Micheal", "M", network);
+		addAUserToNetwork("Kellie", "F", network);
+		addAUserToNetwork("Jack", "M", network);
+		addAUserToNetwork("Donald", "M", network);
+		addAUserToNetwork("Ida", "F", network);
+		addAUserToNetwork("Albert", "M", network);
+		addAUserToNetwork("Rita", "F", network);
+		addAUserToNetwork("Darla", "F", network);
+		addAUserToNetwork("Judith", "F", network);
+		addAUserToNetwork("Hannah", "F", network);
+		addAUserToNetwork("Jake", "M", network);
+		addAUserToNetwork("Clifton", "M", network);
+		
+		connectTwoUsers(8, 2, network);
+		connectTwoUsers(4, 14, network);
+		connectTwoUsers(25, 20, network);
+		connectTwoUsers(4, 22, network);
+		connectTwoUsers(13, 24, network);
+		connectTwoUsers(2, 12, network);
+		connectTwoUsers(6, 16, network);
+		connectTwoUsers(17, 18, network);
+		connectTwoUsers(1, 6, network);
+		connectTwoUsers(18, 20, network);
+		connectTwoUsers(18, 3, network);
+		connectTwoUsers(23, 24, network);
+		connectTwoUsers(6, 15, network);
+		connectTwoUsers(10, 13, network);
+		connectTwoUsers(6, 21, network);
+		connectTwoUsers(12, 13, network);
+		connectTwoUsers(10, 14, network);
+		connectTwoUsers(10, 22, network);
+		connectTwoUsers(13, 23, network);
+		connectTwoUsers(13, 14, network);
+		connectTwoUsers(1, 8, network);
+		connectTwoUsers(1, 2, network);
+		connectTwoUsers(9, 14, network);
+		connectTwoUsers(17, 16, network);
+		connectTwoUsers(11, 2, network);
+		connectTwoUsers(9, 2, network);
+		connectTwoUsers(5, 3, network);
+		connectTwoUsers(22, 23, network);
+		connectTwoUsers(19, 2, network);
+		connectTwoUsers(5, 4, network);
+		connectTwoUsers(6, 4, network);
+		connectTwoUsers(2, 3, network);
+		connectTwoUsers(25, 4, network);
+		connectTwoUsers(20, 5, network);
+		connectTwoUsers(17, 11, network);
+		connectTwoUsers(10, 12, network);
+		connectTwoUsers(8, 9, network);
+		connectTwoUsers(8, 12, network);
+		connectTwoUsers(26, 8, network);
+		connectTwoUsers(16, 18, network);
+		connectTwoUsers(5, 19, network);
+		connectTwoUsers(18, 19, network);
+		connectTwoUsers(2, 5, network);
+		connectTwoUsers(10, 4, network);
+		connectTwoUsers(14, 24, network);
+		connectTwoUsers(1, 7, network);
+		connectTwoUsers(12, 14, network);
+		connectTwoUsers(14, 22, network);
+		connectTwoUsers(8, 7, network);
+		connectTwoUsers(4, 9, network);
+		connectTwoUsers(5, 25, network);
+		connectTwoUsers(8, 10, network);
+		connectTwoUsers(25, 19, network);
+		connectTwoUsers(6, 3, network);
+		connectTwoUsers(25, 9, network);
+		connectTwoUsers(25, 22, network);
+		connectTwoUsers(9, 5, network);
+		connectTwoUsers(18, 11, network);
+		connectTwoUsers(16, 3, network);
+		connectTwoUsers(9, 10, network);
+		connectTwoUsers(2, 4, network);
+
 	}
 
 }
